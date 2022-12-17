277656da04dd0e330ba25e964b8bb1be22dd3fcf
leifgullstad <leifg@umich.edu>
2022-12-16 21:37:07 -0700

    Attempt to fix import error

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import os
 import requests
 
 import datetime
-import os.path
+# import os.path
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
